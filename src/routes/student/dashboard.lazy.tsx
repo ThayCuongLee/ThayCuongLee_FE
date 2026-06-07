dc457e5cb3c75df8205fde4a10a45ceba0ce6ebc
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-06-07 15:39:32 +0700

    feat: add ScrollArea UI component and implement student dashboard lazy route

--- src/routes/student/dashboard.lazy.tsx
+++ src/routes/student/dashboard.lazy.tsx
@@ -1,5 +1,5 @@
-﻿import { createLazyFileRoute } from '@tanstack/react-router'
-import StudentDashboardPage from '../pages/StudentDashboardPage'
+import { createLazyFileRoute } from '@tanstack/react-router'
+import StudentDashboardPage from '../../pages/StudentDashboardPage'
 
 export const Route = createLazyFileRoute('/student/dashboard')({
   component: StudentDashboardPage,
